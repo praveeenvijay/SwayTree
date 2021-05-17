4c1671923c0e8acb9bd9d2f866b161c805a36988
Praveen Vijay <>
2021-05-17 05:59:32 +0000

    swaying tree done!

--- script.js
+++ script.js
@@ -8,18 +8,18 @@ angleMode(DEGREES)
 
 
 }
-let seed = 5
+let seed = 67173.35500009358
 function draw() {
-  background(20, 164, 249)
+  background(165, 203, 198)
   translate(width / 2
   ,height/1.25) 
   randomSeed(seed)
   text(seed,50,50)
-  t=t+0.03
+  t=t+0.01
 
  
   branch(lm)
-  branch(lm-10);
+ // branch(lm-20)
   //branch(lm-25)
   
 
@@ -34,10 +34,10 @@ function mousePressed(){
   
  if (len>ls){
     strokeWeight(map(len,ls,lm,0.1,8))
-    stroke(70,40,20)
+    stroke(70,40,40)
     line(0,0,0, -len)
     translate(0, -len)
-    rotate(random(-10,-30)+15*noise(t))
+    rotate(random(-10,-30)+15*noise(t)-7.5)
     branch(len*random(0.7,0.9)) 
     rotate(random(30,50)+15*noise(t))
     branch(len*random(0.7,0.9))
@@ -49,9 +49,9 @@ function mousePressed(){
       {var r = 0 + random(199,255)
       var g = 0 + random(13,255)
       var b = 0 + random(0,27)
-      fill(r,g,b,170)
+      fill(r,g,b,)
       noStroke()
-     ellipse(0,0,random(8,10))}
+     ellipse(0,0,random(8,15))}
 
      
 
@@ -60,16 +60,18 @@ function mousePressed(){
       var r = 0 + random(50,70)
       var g = 0 + random(42,150)
       var b = 0 + random(67,82)
-      fill(r,g,b,200)
+      fill(r,g,b,)
       noStroke()
-       ellipse(0,0,random(7,10))
+       ellipse(0,0,random(5,10))
       
       
 
       
 
       
-      }
+     }
    }
   pop()
+
+  
  }
